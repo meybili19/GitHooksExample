33c0ea5222f2079888ffe34bd219e85c9585684b
Meybili <meybili200219@gmail.com>
2024-12-13 22:58:10 -0500

    Prueba del hook pre-push

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const http = require("http");
 
 const server = http.createServer((req, res) => {
     res.writeHead(200, { "Content-Type": "text/plain" });
-    res.end("Hello world using Git Hooks testing");
+    res.end("Hello world using Git Hooks");
 });
 
 server.listen(3000, '0.0.0.0', () => {
